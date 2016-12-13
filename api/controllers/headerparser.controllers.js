3642e98279d5878ee130688e490319e92348cc67
Aleksandar Ivanov <alex.arcanum@gmail.com>
2016-12-13 22:16:31 +0100

    added ip, lang, os without checking

--- api/controllers/headerparser.controllers.js
+++ api/controllers/headerparser.controllers.js
@@ -1,3 +1,19 @@
 module.exports.getHeaderParser = function(req, res) {
-  console.log("something");
+
+  var clientIp = req.ip; //connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+  var clientLang = req.headers["accept-language"];
+  var clientOS = req.headers["user-agent"];
+
+  var responseJSON = {
+    "IP Address": clientIp,
+    "Language": clientLang,
+    "OS": clientOS
+  }
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(responseJSON, null, 3));
+
+  console.log(req.headers);
+
+
 };
